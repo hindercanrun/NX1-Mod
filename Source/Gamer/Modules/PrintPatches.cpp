da302bad7ab9d72751324d712bbbd84674a0ab3c
hindercanrun <109132519+hindercanrun@users.noreply.github.com>
2025-10-14 13:32:53 +0800

    exec from disk print

--- Source/Gamer/Modules/PrintPatches.cpp
+++ Source/Gamer/Modules/PrintPatches.cpp
@@ -12,14 +12,25 @@ namespace PrintPatches
 		return Invoke(localClientNum, controllerIndex, filename);
 	}
 
+	Util::Hook::Detour Cmd_ExecFromDisk_Hook;
+	bool Cmd_ExecFromDisk(int localClientNum, int controllerIndex, const char* filename)
+	{
+		Util::Print::Printf("execing %s from disk\n", filename);
+
+		auto Invoke = Cmd_ExecFromDisk_Hook.Invoke<bool(*)(int, int, const char*)>();
+		return Invoke(localClientNum, controllerIndex, filename);
+	}
+
 	void RegisterHooks()
 	{
 		Cmd_ExecFromFastFile_Hook.Create(0x822CA408, Cmd_ExecFromFastFile);
+		Cmd_ExecFromDisk_Hook.Create(0x822CA390, Cmd_ExecFromDisk);
 	}
 
 	void UnregisterHooks()
 	{
 		Cmd_ExecFromFastFile_Hook.Clear();
+		Cmd_ExecFromDisk_Hook.Clear();
 	}
 
 	void Load()
