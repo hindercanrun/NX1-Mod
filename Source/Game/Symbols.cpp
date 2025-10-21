25ffd7cf2e145d3040fd8167059507d9a42ba421
hindercanrun <109132519+hindercanrun@users.noreply.github.com>
2025-10-21 11:58:45 +0800

    SP: remove unused symbols

--- Source/Game/Symbols.cpp
+++ Source/Game/Symbols.cpp
@@ -2,39 +2,29 @@ namespace Symbols
 {
 	namespace SP_Dev
 	{
-		Cmd_AddCommand_t Cmd_AddCommand = Cmd_AddCommand_t(0x822C96F8);
-		Cmd_FindCommand_t Cmd_FindCommand = Cmd_FindCommand_t(0x822C9698);
+		Cmd_AddCommand_t Cmd_AddCommand = Cmd_AddCommand_t(0x824221C8);
 		Cbuf_AddText_t Cbuf_AddText = Cbuf_AddText_t(0x82421160);
 
 		Com_Printf_t Com_Printf = Com_Printf_t(0x82427A50);
 
-		CL_ConsolePrint_t CL_ConsolePrint = CL_ConsolePrint_t(0x8218F058);
 		Con_CheckResize_t Con_CheckResize = Con_CheckResize_t(0x822072D0);
 		Con_DrawSolidConsole_t Con_DrawSolidConsole = Con_DrawSolidConsole_t(0x8220E9D0);
-
-		DB_AuthLoad_InflateInit_t DB_AuthLoad_InflateInit = DB_AuthLoad_InflateInit_t(0x821CC670);
-		DB_LoadXAssets_t DB_LoadXAssets = DB_LoadXAssets_t(0x82213570);
-
-		Dvar_ValueToString_t Dvar_ValueToString = Dvar_ValueToString_t(0x82372F70);
-		Dvar_IsCheatProtected_t Dvar_IsCheatProtected = Dvar_IsCheatProtected_t(0x82372920);
 
 		Key_IsCatcherActive_t Key_IsCatcherActive = Key_IsCatcherActive_t(0x82217FB0);
 
 		R_AddCmdDrawText_t R_AddCmdDrawText = R_AddCmdDrawText_t(0x826FCB00);
-
-		Sys_Milliseconds_t Sys_Milliseconds = Sys_Milliseconds_t(0x823A6C20);
-
-		Structs::DBLoadData* g_load = (Structs::DBLoadData*)0x8283DA90;
 
 		Structs::ScreenPlacement* scrPlaceFull = (Structs::ScreenPlacement*)0x82B15C08;
 	}
 
 	namespace MP_Dev
 	{
+		// TODO
 	}
 
 	namespace SP_Demo
 	{
+		// TODO
 	}
 
 	namespace MP_Demo
--- Source/Game/Symbols.h
+++ Source/Game/Symbols.h
@@ -7,17 +7,11 @@ namespace Symbols
 		typedef void (*Cmd_AddCommand_t)(const char* cmdName, void (__cdecl* function)(), Structs::cmd_function_s* allocedCmd);
 		extern Cmd_AddCommand_t Cmd_AddCommand;
 
-		typedef Structs::cmd_function_s* (*Cmd_FindCommand_t)(const char* cmdName);
-		extern Cmd_FindCommand_t Cmd_FindCommand;
-
 		typedef void (*Cbuf_AddText_t)(int localClientNum, const char* text);
 		extern Cbuf_AddText_t Cbuf_AddText;
 
 		typedef void (*Com_Printf_t)(int channel, const char* fmt, ...);
 		extern Com_Printf_t Com_Printf;
-
-		typedef void (*CL_ConsolePrint_t)(int localClientNum, int channel, const char* txt, int duration, int pixelWidth, int flags);
-		extern CL_ConsolePrint_t CL_ConsolePrint;
 
 		typedef void (*Con_CheckResize_t)(const Structs::ScreenPlacement* scrPlace);
 		extern Con_CheckResize_t Con_CheckResize;
@@ -25,38 +19,23 @@ namespace Symbols
 		typedef void (*Con_DrawSolidConsole_t)(int localClientNum);
 		extern Con_DrawSolidConsole_t Con_DrawSolidConsole;
 
-		typedef HRESULT (*DB_AuthLoad_InflateInit_t)(Structs::db_z_stream_s* stream, int isSecure, const char* filename);
-		extern DB_AuthLoad_InflateInit_t DB_AuthLoad_InflateInit;
-
-		typedef void (*DB_LoadXAssets_t)(Structs::XZoneInfo* zoneInfo, unsigned int zoneCount, unsigned int syncMode);
-		extern DB_LoadXAssets_t DB_LoadXAssets;
-
-		typedef const char* (*Dvar_ValueToString_t)(const Structs::dvar_t* dvar, Structs::DvarValue value);
-		extern Dvar_ValueToString_t Dvar_ValueToString;
-
-		typedef int (*Dvar_IsCheatProtected_t)(unsigned short flags);
-		extern Dvar_IsCheatProtected_t Dvar_IsCheatProtected;
-
 		typedef bool (*Key_IsCatcherActive_t)(int localClientNum, int mask);
 		extern Key_IsCatcherActive_t Key_IsCatcherActive;
 
 		typedef void (*R_AddCmdDrawText_t)(const char* text, int maxChars, Structs::Font_s* font, float x, float y, float xScale, float yScale, float rotation, const float* colour, int style, Structs::EScreenLayer layer);
 		extern R_AddCmdDrawText_t R_AddCmdDrawText;
 
-		typedef DWORD (*Sys_Milliseconds_t)();
-		extern Sys_Milliseconds_t Sys_Milliseconds;
-
-		extern Structs::DBLoadData* g_load;
-
 		extern Structs::ScreenPlacement* scrPlaceFull;
 	}
 
 	namespace MP_Dev
 	{
+		// TODO
 	}
 
 	namespace SP_Demo
 	{
+		// TODO
 	}
 
 	namespace MP_Demo
@@ -82,7 +61,7 @@ namespace Symbols
 		typedef const char* (*Dvar_ValueToString_t)(const Structs::dvar_t* dvar, Structs::DvarValue value);
 		extern Dvar_ValueToString_t Dvar_ValueToString;
 
-		typedef int (*Dvar_IsCheatProtected_t)(unsigned __int16 flags);
+		typedef int (*Dvar_IsCheatProtected_t)(unsigned short flags);
 		extern Dvar_IsCheatProtected_t Dvar_IsCheatProtected;
 
 		typedef DWORD (*Sys_Milliseconds_t)();
